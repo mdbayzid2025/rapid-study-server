6ed00f93dfc66aa5dfb24eeabc4f3e369cfa31b8
mdbayzid2025 <ssmdbayzid23@gmail.com>
2025-09-01 19:16:28 +0600

    sadff

--- config/index.js
+++ config/index.js
@@ -25,5 +25,22 @@ CLOUDINARY_API_KEY = 952383858859415
 
 CLOUDINARY_SECRET_KEY = tR2hJQxUnXAibVDVP2eBtjbZCFk
 
+------------------------- Sep 25 ---------------
+
+MONGO_URL = mongodb+srv://ssmdbayzid23:uYmWXBF57f9HHwro@cluster0.nvuq86p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+
+port = 5000
+user = ssmdbayzid23
+db_password =  uYmWXBF57f9HHwro
+ORIGIN = http://localhost:3000
+BASE_URL = http://localhost:5000
+
+CLOUDINARY_CLOUD_NAME = detjedqvq 
+
+CLOUDINARY_API_KEY = 952383858859415
+
+CLOUDINARY_SECRET_KEY = tR2hJQxUnXAibVDVP2eBtjbZCFk
+
+
 
 */
